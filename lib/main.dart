ef4584f057df776e027dc93e9bfef2ced99bb221
afiqaimanafr <aaafr98@gmail.com>
2021-10-12 15:29:49 +0800

    latest update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
 
+import './quiz.dart';
+import './result.dart';
 // void main() {
 //   runApp(MyApp());
 // }
@@ -17,55 +17,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
-  final questions = const [
+  final _questions = const [
     {
-      'questionText': 'What is Afiq\'s favourite colour?',
-      'answers': ['Red', 'Blue', 'Green', 'Black'],
+      'questionText': 'What is your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
-      'questionText': 'What is Afiq\'s favourite animal?',
-      'answers': ['Cat', 'Dog', 'Rabbit', 'Tiger'],
+      'questionText': 'What is your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Cat', 'score': 11},
+        {'text': 'Tiger', 'score': 5},
+        {'text': 'Monkey', 'score': 9},
+      ],
     },
     {
-      'questionText': 'What is Afiq\'s hobby?',
-      'answers': ['Jogging', 'Reading', 'Coding', 'Sleeping'],
+      'questionText': 'What is your hobby?',
+      'answers': [
+        {'text': 'Reading', 'score': 9},
+        {'text': 'Jogging', 'score': 2},
+        {'text': 'Coding', 'score': 1},
+        {'text': 'Sleeping', 'score': 10},
+      ],
     },
   ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
-      print('No more question');
+      print('No more questions!');
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(
-                    questions[_questionIndex]['questionText'],
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(child: Text('You did it!')),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
